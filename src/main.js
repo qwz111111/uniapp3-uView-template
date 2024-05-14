442ab51c6f204de68de62c4a1153266bf3c4a325
CompanyComputers <2310860596@qq.com>
2024-05-14 15:06:40 +0800

    修改uview默认配置

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createSSRApp } from 'vue'
 import uviewPlus from 'uview-plus'
 import App from './App.vue'
-import uviewPlusConfig from './utils/uviewPlusConfig.js'
+import './utils/uviewPlusConfig.js'
 // ios联系触摸放大问题
 // #ifdef H5
 import './utils/ios.js'
@@ -10,6 +10,5 @@ import './utils/ios.js'
 export function createApp() {
   const app = createSSRApp(App)
   app.use(uviewPlus)
-  uviewPlusConfig()
   return { app }
 }
